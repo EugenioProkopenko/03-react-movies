a7b29f6e8b829c101ee41224d16f3d244d83f020
ievgenii_prokopenko <ievgeniiprokopenko@gmail.com>
2026-05-12 13:26:08 +0200

    fix build

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,12 @@
 import SearchBar from '../SearchBar/SearchBar';
-import type { Movie } from '../../types/movie';
+//import type { Movie } from '../../types/movie';
 
 export default function App() {
   //const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
   const handleSearch = () => {};
   return (
-    <>
+    <div>
       <SearchBar onSubmit={handleSearch} />
-    </>
+    </div>
   );
 }
